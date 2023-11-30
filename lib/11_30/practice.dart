048c8dbbc504489300d9f615a3dac29ad7683a7f
dodocap <dodocap@naver.com>
2023-11-30 16:41:11 +0900

    연습문제 1-3

--- lib/11_30/practice.dart
+++ lib/11_30/practice.dart
@@ -6,4 +6,14 @@ class Cleric {
   int mp = 10;
 
   Cleric(this.name);
+
+  selfAid() {
+    if(mp < 5) {
+      print('셀프 에이드를 사용하려면 mp가 5 이상이어야 합니다');
+      return;
+    }
+    mp -= 5;
+    hp = maxHp;
+    print('셀프 에이드 시전 HP : $hp MP : $mp');
+  }
 }
