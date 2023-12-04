146fa28eadebff7c725ee8cfe91b34f82aa0238f
dodocap <dodocap@naver.com>
2023-12-04 15:03:52 +0900

    연습문제 2-1

---
+++ lib/23_12_04/practice_2/question_1.dart
@@ -0,0 +1,16 @@
+void main() {
+  // 1) 대한민국의 도시 이름 모음 (순서 상관 없음)
+  final Set<String> citiesOfKorea = {
+    // '도시명' ...
+  };
+
+  // 2) 10명 학생의 시험 점수
+  final Map<String, int> scoreOfStudents = {
+    // '학생이름' : 점수
+  };
+
+  // 3) 대한민국의 도시별 인구수 (순서 상관 없음)
+  final Map<String, int> populationOfCities = {
+    // '도시명' : 인구수
+  };
+}
