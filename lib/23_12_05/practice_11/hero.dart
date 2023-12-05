14569b2a27f895852b5aa65c52f932d8125e208d
dodocap <dodocap@naver.com>
2023-12-05 13:57:15 +0900

    사전 클래스 정의(연습문제 11-3)

---
+++ lib/23_12_05/practice_11/hero.dart
@@ -0,0 +1,29 @@
+import 'slime.dart';
+
+class Hero {
+  String name;
+  int _hp;
+
+  Hero({
+    required this.name,
+    required int hp,
+  }) : _hp = hp;
+
+  int get hp => _hp;
+
+  set hp(int value) {
+    if (value < 0) {
+      throw Exception('hp는 0보다 작을 수 없다');
+    }
+    _hp = value;
+  }
+
+  void attack(Slime slime) {
+    slime.hp -= 10;
+    print('10의 데미지!!!');
+  }
+
+  void run() {
+    print('뛰었다');
+  }
+}
