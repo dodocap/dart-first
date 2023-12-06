088d27fa7f5f16eedc21e7fd56bc71e8283b494c
dodocap <dodocap@naver.com>
2023-12-06 20:36:51 +0900

    연습문제 13-6

---
+++ lib/23_12_06/practice_13_starcraft/group.dart
@@ -0,0 +1,32 @@
+import 'interfaces.dart';
+
+class Group {
+  final List<Unit> _groupList = [];
+
+  void clickUnit(Unit unit) {
+    _printGroupState();
+
+    if (_groupList.contains(unit)) {
+      _groupList.remove(unit);
+      print('[삭제] [${unit.name}(${unit.hp})]');
+    } else {
+      if(_groupList.length < 12) {
+        _groupList.add(unit);
+        print('[추가] [${unit.name}(${unit.hp})]');
+      } else {
+        print('[실패] [${unit.name}(${unit.hp})]');
+      }
+    }
+
+    _printGroupState();
+    print('--------------------------------------------------------------');
+  }
+
+  void _printGroupState() {
+    final buffer = StringBuffer();
+    for(final unit in _groupList) {
+      buffer.write('[${unit.name}(${unit.hp})] ');
+    }
+    print('인원(${_groupList.length}) - $buffer');
+  }
+}
--- lib/23_12_06/practice_13_starcraft/test.dart
+++ lib/23_12_06/practice_13_starcraft/test.dart
@@ -1,12 +1,18 @@
+import 'group.dart';
+import 'interfaces.dart';
 import 'terran/scv.dart';
 import 'zerg/drone.dart';
 
 void main() {
-  final scv = SCV(name: 'SCV', hp: 10, damage: 5);
+  final group = Group();
+  List<Unit> units = [];
+  for(int i =0; i<12; i++){
+    final drone = Drone(name: 'Drone$i', hp: (i+1) * 10, damage: 5);
+    units.add(drone);
+    group.clickUnit(drone);
+  }
 
-  final drone = Drone(name: 'Drone', hp: 10, damage: 5);
-
-  print(drone);
-  scv.attack(drone);
-  print(drone);
+  group.clickUnit(SCV(name: 'SCV', hp: 10, damage: 5));
+  group.clickUnit(units[0]);
+  group.clickUnit(SCV(name: 'SCV', hp: 10, damage: 5));
 }
