46135aa7202a16e47ab8b5b27185a22e3915ade4
dodocap <dodocap@naver.com>
2023-12-08 18:26:17 +0900

    실습 2

--- lib/23_12_08/word.dart
+++ lib/23_12_08/word.dart
@@ -16,13 +16,33 @@ class Word {
     }
     return isVowel;
   }
+
+  bool isConsonant(int i) {
+    // 방법 1
+    return !isVowel(i);
+
+    // 방법 2
+    /*bool isConsonant;
+    switch (word.substring(i, i + 1).toLowerCase()) {
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+        isConsonant = false;
+        break;
+      default:
+        isConsonant = true;
+    }
+    return isConsonant;*/
+  }
 }
 
 void main() {
   final word = Word();
-  word.word = 'flutter';
+  word.word = 'Flutter';
 
   for (int i = 0; i < word.word.length; i++) {
-    print('${word.word[i]} : 모음(${word.isVowel(i) ? 'O' : 'X'})');
+    print('${word.word[i]} : 모음(${word.isVowel(i) ? 'O' : 'X'}) / 자음(${word.isConsonant(i) ? 'O' : 'X'})');
   }
 }
