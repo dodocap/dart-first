c35891b46e510a7109813afecfd1b2386567d53e
dodocap <dodocap@naver.com>
2023-12-11 14:32:55 +0900

    연습문제 2

--- lib/23_12_11/practice_exception.dart
+++ lib/23_12_11/practice_exception.dart
@@ -1,9 +1,12 @@
 void main() {
   final numString = '10.5';
 
-  // Unhandled exception:
-  // FormatException: Invalid radix-10 number (at character 1)
-  int num = int.parse(numString);
+  int num;
+  try {
+    num = int.parse(numString);
+  } catch(e) {
+    num = 0;
+  }
 
   print(num);
 }
