af2dda5c5d3835b0dc908515355bac43a3790ef7
dodocap <dodocap@naver.com>
2023-12-12 09:34:45 +0900

    연습문제 1 포맷팅 및 '지식이 늘었다' 코드 추가

--- lib/23_12_11/practice_exception.dart
+++ lib/23_12_11/practice_exception.dart
@@ -1,12 +1,22 @@
 void main() {
   final numString = '10.5';
 
+  print(solution1(numString));
+  print(solution2(numString));
+}
+
+int solution1(String numString) {
   int num;
   try {
     num = int.parse(numString);
-  } catch(e) {
+  } catch (e) {
     num = 0;
   }
 
-  print(num);
+  return num;
+}
+
+int solution2(String numString) {
+  // 지식이 늘었다
+  return int.tryParse(numString) ?? 0;
 }
